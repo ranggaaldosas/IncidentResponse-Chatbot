58a19210d7438b39215d2252aa51fca2f10dd9e4
dzakiruw <dzakiruw@gmail.com>
2024-06-11 23:59:57 +0700

    feat: member

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,9 +3,13 @@ import ChatSection from "./components/chat-section";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-10 p-24 background-gradient">
-      <Header />
-      <ChatSection />
+    <main className="flex min-h-screen background-gradient">
+      <div className="w-1/4 flex flex-col items-center p-4">
+        <Header />
+      </div>
+      <div className="flex-grow flex flex-col items-center gap-10 p-24">
+        <ChatSection />
+      </div>
     </main>
   );
 }
